619b23620fb4ed6546cf3741e63170674f82dd59
Gopal Varshney <128905415+GopalVar2005@users.noreply.github.com>
2025-08-03 17:01:03 +0530

    Create 02_datatypes.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as newer version of JavaScript
+
+// alert(3+3); // shows an alert box with the result of 3+3, not here bcs it work only in browser
+
+let name = "John"; // string
+let age = 30; // number
+let isStudent = true; // boolean
+
+// numer-> 2 to power 53
+// bigInt
+// string-> ""
+// boolean-> true/false
+// null-> standalone value(khali value)
+// undefined-> variable that has been declared but not assigned a value
+// symbol-> unique and immutable value
+
+//object
+
+console.log(typeof name);
+console.log(typeof 12);
+console.log(typeof null); // null is an object in JavaScript, this is a known bug
+console.log(typeof undefined); // undefined is a type of its own
