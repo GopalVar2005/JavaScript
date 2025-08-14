dec37a1309f305330bc99d4ed327c85659cfd302
Gopal Varshney <128905415+GopalVar2005@users.noreply.github.com>
2025-08-14 12:15:07 +0530

    Add files via upload

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,49 @@
+// Objects ko declare krne ke 2 trike ha-> Literal(Singleton nhi bnega), Constructor(Singleton bnega)
+// Singleton-> Apne trh ka 1 hi object
+
+//Constructor
+//Object.create
+
+// Literal Declaration
+// By-default: key->String, value->kuch bhi
+
+const mySym=Symbol("key1");
+
+const JsUser={
+    name:"Gopal",
+    "full name":"Gopal Varshney",
+    [mySym]:"myKey1", // to refer to String otherwise it refer to String
+    age:20,
+    location:"Mathura",
+    email:"gopal.varshney13@gmail.com",
+    isLoggedIn: false,
+    lastoginDays:["Monday","Wednesday","Thursday"]
+}
+
+// Normally
+//console.log(JsUser.email);  
+// Problem
+//console.log(JsUser."full name"); // Error
+
+// Solution
+// console.log(JsUser[email]); // Error, because key is by-default String
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email="gopal.varshney13@google.com";
+// console.log(JsUser);
+// Object.freeze(JsUser);
+// JsUser.email="gopal.varshney13@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("Hello");
+}
+JsUser.greeting2=function(){
+    console.log(`Hello, ${this.name}`); // jab same object ko refer kro
+}
+console.log(JsUser["greeting"]); // function execute nhi hua bas reference aya
+console.log(JsUser.greeting); // // function execute nhi hua bas reference aya
+console.log(JsUser.greeting());
+
+console.log(JsUser.greeting2());
