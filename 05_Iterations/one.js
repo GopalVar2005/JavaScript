d1c6a162edbbe4a0ac5af0e7c3bf2c76ece2598c
Gopal Varshney <128905415+GopalVar2005@users.noreply.github.com>
2025-08-27 22:43:41 +0530

    Create one.js

---
+++ 05_Iterations/one.js
@@ -0,0 +1,27 @@
+// for
+// Ctrl+F2-> Select all occurences
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(i);
+// }
+ 
+// const array=[1,2,3,4,5];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element); 
+// }   
+
+// for(let i=0;i<1;i++)
+// {
+//     for(let j=0;j<2;j++)
+//     {
+//         console.log(i+","+j);
+//     }
+// }
+
+// break, continue
+for (let index = 1; index < 10; index++) {
+    if(index==5) continue;
+    if(index==7) break;
+    console.log(index);
+}
