9fa40f01e0c0e8927b69128eb98666b123e00ea4
Gopal Varshney <128905415+GopalVar2005@users.noreply.github.com>
2025-08-28 22:30:02 +0530

    Add files via upload

---
+++ 05_Iterations/five.js
@@ -0,0 +1,39 @@
+// for-each(callback-> no function name)
+const arr=[1,2,3,4,5]; 
+
+// arr.forEach( function (item){
+//     console.log(item);
+// })
+
+// arr.forEach((item)=>{
+//     console.log(item);
+// })
+
+// const print=function(item)
+// {
+//     console.log(item);
+// }
+// arr.forEach(print); // bas reference dena ha    
+
+arr.forEach((item, index, arr)=>{
+    console.log(item, index, arr);
+})
+
+// Database values
+const values=[
+    {
+        name:"javascript",
+        fileName:"js"
+    },
+    {
+        name:"java",
+        fileName:"java"
+    },
+    {
+        name:"python",
+        fileName:"py"
+    }
+]
+values.forEach((item)=>{
+    console.log(item.name);
+})
